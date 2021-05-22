e15f0a32b734d3fa170489c9b3737222ddde4c68
MGutierrez1998 <55570315+MGutierrez1998@users.noreply.github.com>
2021-05-22 23:36:29 +1000

    Gerald's Home Page and CSS
    added Geralds work

--- src/Website.js
+++ src/Website.js
@@ -6,6 +6,7 @@ function Website() {
   const [nav,setNav] = useState("Home");
 
   return (nav === "Home" ?
+  <div>
     <div className="container">
       <div className="nav-wrapper">
         <div className="left-side">
@@ -25,8 +26,47 @@ function Website() {
           </div>
         </div>
       </div>
-      <h1><p className="homeInfo">This is where the other graphs are located.</p></h1>
-      <p className="nav-link-wrapper"><button className="websiteButton" onClick={() => setNav("Company")}>See Alphabet (Google) Information</button></p>
+    </div>
+    <div className="background">	
+      <div>
+          <div style={{"width": "100%"}}>
+              <img alt="" className="stock-image" style={{"float":"right"}} />
+          </div>
+
+          <div>
+            <div className="homeInfo">
+            <h1>Welcome</h1>
+          </div>
+      
+          <section className ="home_about">
+            <h2>Investing For Everyone</h2>
+          </section>
+
+            <section className="home_about">
+            <h3>Commission-free, plus the tools that you need to put your money in motion.
+            <br/><br/>
+            Created by University of Technology Students for the ease of access that most stock trading website don't give to you.</h3>
+          </section>
+          </div>
+      </div>
+
+      <br/>
+      <div>	
+        <p className="nav-link-wrapper google_link"><button className="websiteButton" onClick={() => setNav("Company")}>See Alphabet (Google) Information</button></p>
+      </div>
+      <br/>
+      <br/>
+      <br/>
+      <br/>
+      <br/>
+      <br/>
+      <br/>
+      <br/>
+      <br/>
+      <br/>
+      <br/>
+      <br/>
+    </div>
     </div>
     : nav === "About" ? 
       <div className="container">
@@ -42,15 +82,28 @@ function Website() {
               <button className="websiteButton" onClick={() => setNav("App")}>App</button>
             </div>
           </div>
-          <div class="right-side">
-            <div class="brand">
+          <div className="right-side">
+            <div className="brand">
               The Stock Trading Helper
             </div>
           </div>
         </div>
-        <div className="profile-content-wrapper">
+        <div className="background">
           <h1><p className="aboutInfo">Hello</p></h1>
           <h3><p className="aboutInfo">This is website allows people to look at stocks and helps predict when to buy and sell, while also giving useful indicators of a companies health.</p></h3>
+        <br/>
+        <br/>
+        <br/>
+        <br/>
+        <br/>
+        <br/>
+        <br/>
+        <br/>
+        <br/>
+        <br/>
+        <br/>
+        <br/>
+        <br></br>
         </div>
       </div>
     : nav === "App" ? <App setNav={setNav} />
